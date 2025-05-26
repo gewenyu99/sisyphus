bf41402222d450cc8e201665a6db61b643964a84
Vincent Ge <vincent@52:1d:06:1c:60:2f.home>
2025-05-26 12:20:09 -0400

    Add posthog.js

--- src/composable/usePosthog.js
+++ src/composable/usePosthog.js
@@ -3,7 +3,7 @@ import posthog from 'posthog-js'
 
 export function usePostHog() {
   posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
-    api_host: 'https://us.i.posthog.com',
+    api_host: import.meta.env.VITE_POSTHOG_HOST,
     capture_pageview: 'history_change'
   })
 
